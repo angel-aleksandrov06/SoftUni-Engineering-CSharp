2fb096b5fe8ddfebe58b950463360f4861a985d4
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-06-26 16:12:10 +0300

    Add Mid Exam 16 April 2019

---
+++ CSharp Fundamentals/Exams/Mid Exam - 16 April 2019/03. Easter Shopping/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Easter_Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Shops = Console.ReadLine().Split(" ").ToList();
+
+            int couontCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < couontCommands; i++)
+            {
+                string[] command = Console.ReadLine().Split(" ");
+
+                
+                if(command[0] == "Include")
+                {
+                    Shops.Add(command[1]);
+                }
+
+                else if (command[0] == "Visit")
+                {
+                    int numberOfShops = int.Parse(command[2]);
+                    
+                    if(numberOfShops>=0 && numberOfShops <= Shops.Count)
+                    {
+                        if (command[1] == "first")
+                        {
+                            for (int j = 0; j < numberOfShops; j++)
+                            {
+                                Shops.RemoveAt(0);
+                            }
+                        }
+                        else if (command[1] == "last")
+                        {
+                            for (int k = 0; k < numberOfShops; k++)
+                            {
+                                Shops.RemoveAt(Shops.Count - 1);
+                            }
+                        }
+                    }
+                }
+
+                else if(command[0] == "Prefer")
+                {
+                    int firstShopIndex = int.Parse(command[1]);
+                    int secondShopIndex = int.Parse(command[2]);
+
+                    bool firstIndexIsRight = firstShopIndex >= 0 && firstShopIndex < Shops.Count;
+                    bool secondIndexIsRight = secondShopIndex >= 0 && secondShopIndex < Shops.Count;
+
+                    if(firstIndexIsRight && secondIndexIsRight)
+                    {
+                        string firtsShop = Shops[firstShopIndex];
+                        string secondShop = Shops[secondShopIndex];
+
+                        Shops[firstShopIndex] = secondShop;
+                        Shops[secondShopIndex] = firtsShop;
+                    }
+                }
+
+                else if(command[0] == "Place")
+                {
+                    int numberOfShops = int.Parse(command[2]);
+
+                    if (numberOfShops >= 0 && numberOfShops <= Shops.Count)
+                    {
+                        Shops.Insert(numberOfShops + 1, command[1]);
+                    }
+                }
+            }
+
+            Console.WriteLine("Shops left:");
+            Console.WriteLine(string.Join(" ",Shops));
+        }
+    }
+}
---
+++ CSharp Fundamentals/Exams/Mid Exam - 16 April 2019/Problem 1. Easter Cozonacs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Problem_1._Easter_Cozonacs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal budget = decimal.Parse(Console.ReadLine());
+            decimal priceOfOneKgFloor = decimal.Parse(Console.ReadLine());
+
+            decimal priceOfOnePackEggs = 0.75M * priceOfOneKgFloor;
+            decimal priceOfOneLMilk = priceOfOneKgFloor + (0.25M * priceOfOneKgFloor);
+            decimal priceOf250mlMilk = priceOfOneLMilk / 4;
+
+            int counterOfCozonacs = 0;
+
+            decimal priceForOneCozonac = priceOfOneKgFloor + priceOfOnePackEggs + priceOf250mlMilk;
+
+            int coloredEggs = 0;
+            decimal moneyLeft = 0;
+
+            while (budget >= 0)
+            {
+                decimal leftMoney = budget - priceForOneCozonac;
+                if (leftMoney <= 0)
+                {
+                    moneyLeft = budget;
+                    break;
+                }
+                else
+                {
+                    budget = leftMoney;
+                    counterOfCozonacs++;
+                    coloredEggs += 3;
+                }
+                if (counterOfCozonacs % 3 == 0 && counterOfCozonacs!=0)
+                {
+                    coloredEggs = coloredEggs - (counterOfCozonacs - 2);
+                }
+
+            }
+
+            Console.WriteLine($"You made {counterOfCozonacs} cozonacs! Now you have {coloredEggs} eggs and {moneyLeft:F2}BGN left.");
+        }
+    }
+}
