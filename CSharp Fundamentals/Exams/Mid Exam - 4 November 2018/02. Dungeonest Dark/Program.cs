629ce06e264bd7d570e9ace241aab83023983fdf
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-06-28 15:48:41 +0300

    add Mid Exam - 4 November 2018

---
+++ CSharp Fundamentals/Exams/Mid Exam - 4 November 2018/02. Dungeonest Dark/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace _02._Dungeonest_Dark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rooms = Console.ReadLine().Split("|");
+
+            int health = 100;
+            int coins = 0;
+
+
+            bool isYouDead = false;
+
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                string[] commandArgs = rooms[i].Split(" ");
+
+                string @event = commandArgs[0];
+                int number = int.Parse(commandArgs[1]);
+
+                if (@event == "potion")
+                {
+                    int newEnergy = number;
+
+                    if (health + newEnergy > 100)
+                    {
+                        int recivedHealth = 100 - health;
+                        health += recivedHealth;
+                        Console.WriteLine($"You healed for {recivedHealth} hp.");
+                        Console.WriteLine($"Current health: {health} hp.");
+                    }
+                    else
+                    {
+                        health += newEnergy;
+                        int recivedEnergy = newEnergy;
+                        Console.WriteLine($"You healed for {recivedEnergy} hp.");
+                        Console.WriteLine($"Current health: {health} hp.");
+                    }
+                }
+                else if (@event == "chest")
+                {
+                    int recivedCoins = number;
+                    coins += recivedCoins;
+
+                    Console.WriteLine($"You found {recivedCoins} coins.");
+                    
+                }
+                else
+                {
+                    int powerOfMonster = number;
+                    string monster = @event;
+
+                    health -= powerOfMonster;
+
+                    if (health > 0)
+                    {
+                         Console.WriteLine($"You slayed {monster}.");
+                    }
+                    else
+                    {
+                        int room = i +1;
+                        Console.WriteLine($"You died! Killed by {monster}.");
+                        Console.WriteLine($"Best room: {room}");
+
+                        isYouDead = true;
+                        break;
+                    }
+
+                }
+            }
+
+            if (isYouDead == false)
+            {
+                Console.WriteLine($"You've made it!");
+                Console.WriteLine($"Coins: {coins}");
+                Console.WriteLine($"Health: {health}");
+            }
+
+        }
+    }
+}
---
+++ CSharp Fundamentals/Exams/Mid Exam - 4 November 2018/03. Quests Journal/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Quests_Journal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split(", ").ToList();
+
+            string command = Console.ReadLine();
+
+            while (command!= "Retire!")
+            {
+                string[] inputCommands = command.Split(" - ");
+
+                string type = inputCommands[0];
+                string quest = inputCommands[1];
+
+                bool isQuestExist = input.Contains(quest);
+
+                if (type == "Start" && isQuestExist==false)
+                {
+                     input.Add(quest);
+                }
+
+                else if(type == "Complete" && isQuestExist == true)
+                {
+                    for (int i = 0; i < input.Count; i++)
+                    {
+                        if (input.Contains(quest))
+                        {
+                            input.Remove(quest);
+                            i--;
+                        }
+                    }
+                }
+
+                else if(type == "Side Quest")
+                {
+                    string[] sideQuestArray = quest.Split(":");
+                    string newQuest = sideQuestArray[0];
+                    string sideQuest = sideQuestArray[1];
+                    bool isSideQuestExist = input.Contains(sideQuest);
+
+                    if (input.Contains(newQuest) && isSideQuestExist == false)
+                    {
+                        int index = input.IndexOf(newQuest);
+                        input.Insert(index + 1, sideQuest);
+                    }
+                }
+
+                else if(type == "Renew" && isQuestExist==true)
+                {
+                    input.Remove(quest);
+                    input.Add(quest);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", input));
+        }
+    }
+}
