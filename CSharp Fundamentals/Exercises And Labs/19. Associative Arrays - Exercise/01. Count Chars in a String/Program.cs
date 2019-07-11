6457f883c1c2f66f5ebee82069342e7b40b27273
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-07-11 23:13:02 +0300

    add Associative Arrays - Exercise

---
+++ CSharp Fundamentals/Exercises And Labs/19. Associative Arrays - Exercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace _01._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someText = Console.ReadLine();
+
+            Dictionary<char, int> lettersCount = new Dictionary<char, int>();
+
+            for (int i = 0; i < someText.Length; i++)
+            {
+                char letter = someText[i];
+
+                if(letter != ' ')
+                {
+                    if (!lettersCount.ContainsKey(letter))
+                    {
+                        lettersCount[letter] = 1;
+                    }
+                    else
+                    {
+                        lettersCount[letter]++;
+                    }
+                }
+            }
+
+            foreach(var kvp in lettersCount)
+            {
+                char key = kvp.Key;
+                int value = kvp.Value;
+
+                Console.WriteLine($"{key} -> {value}");
+            }
+        }
+    }
+}
---
+++ CSharp Fundamentals/Exercises And Labs/19. Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            Dictionary<string, int> pairs = new Dictionary<string, int>();
+
+            while (command != "stop")
+            {
+                string key = command;
+                int value = int.Parse(Console.ReadLine());
+
+                pairs[key] = value;
+
+                command = Console.ReadLine();
+
+                if (!key.Contains(pairs.Keys))
+                {
+                    if (!lettersCount.ContainsKey(letter))
+                    {
+                        lettersCount[letter] = 1;
+                    }
+                    else
+                    {
+                        lettersCount[letter]++;
+                    }
+                }
+            }
+
+            foreach (var kvp in pairs)
+            {
+                string key = kvp.Key;
+                int value = kvp.Value;
+
+                Console.WriteLine($"{key} -> {value}");
+            }
+        }
+    }
+}
