bf4e10f243b9ad435daf1a7ee7ee19dcb95df994
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-07-31 22:31:41 +0300

    add "Final Exam Retake 20-Dec-2018"

---
+++ CSharp Fundamentals/Exams/Fundamentals Retake Final Exam - 20 December 2018/01. Vapor Winter Sale/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Vapor_Winter_Sale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var dictGamePrice = new Dictionary<string, double>();
+            var dictDLC = new Dictionary<string, string>();
+
+            var dictGamePrice2 = new Dictionary<string, double>();
+
+            var splitedInputGames = input.Split(", ");
+
+            foreach (var game in splitedInputGames)
+            {
+                if (game.Contains('-'))
+                {
+                    var splitedGame = game.Split("-");
+                    var nameGame = splitedGame[0];
+                    double priceofGame = double.Parse(splitedGame[1].ToString());
+
+                    if (!dictGamePrice.ContainsKey(nameGame))
+                    {
+                        dictGamePrice[nameGame] = priceofGame;
+                    }
+                }
+                else if (game.Contains(':'))
+                {
+                    var splitedGame = game.Split(":");
+                    var nameGame = splitedGame[0];
+                    var nameDLC = splitedGame[1];
+
+                    if (dictGamePrice.ContainsKey(nameGame))
+                    {
+                        if (!dictDLC.ContainsKey(nameGame))
+                        {
+                            dictDLC[nameGame] = nameDLC;
+
+                            dictGamePrice[nameGame] *= 1.20;
+                        }
+                    }
+                }
+            }
+
+            foreach (var game in dictGamePrice)
+            {
+                var onmind = 0.0;
+                if (dictDLC.ContainsKey(game.Key))
+                {
+                    onmind = dictGamePrice[game.Key]*0.50;
+                }
+                else
+                {
+                    onmind = dictGamePrice[game.Key] * 0.80;
+                }
+
+                dictGamePrice2[game.Key] = onmind;
+            }
+
+            foreach (var item in dictGamePrice2.OrderBy(x=>x.Value))
+            {
+                if (dictDLC.ContainsKey(item.Key))
+                {
+                    Console.WriteLine($"{item.Key} - {dictDLC[item.Key]} - {item.Value:F2}");
+                }
+            }
+
+            foreach (var item in dictGamePrice2.OrderByDescending(x => x.Value))
+            {
+                if (!dictDLC.ContainsKey(item.Key))
+                {
+                    Console.WriteLine($"{item.Key} - {item.Value:F2}");
+                }
+            }
+        }
+    }
+}
---
+++ CSharp Fundamentals/Exams/Fundamentals Retake Final Exam - 20 December 2018/02. Activation Keys/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02._Activation_Keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var list = new List<string>();
+
+            var splitedInput = input.Split("&", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var item in splitedInput)
+            {
+                var length = item.Length;
+                bool isSymbolsValid = true;
+                bool islengthValid = false;
+
+                if (length == 16 || length == 25)
+                {
+                    islengthValid = true;
+                    for (int i = 0; i < length; i++)
+                    {
+                        if (!Char.IsLetterOrDigit(item[i]))
+                        {
+                            isSymbolsValid = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (islengthValid && isSymbolsValid)
+                {
+
+                    if (length == 16)
+                    {
+                        var sb = new StringBuilder();
+
+                        if (char.IsLetter(item[0]))
+                        {
+                            var newSymbol = item[0].ToString().ToUpper();
+                            sb.Append(newSymbol);
+                        }
+                        if (char.IsDigit(item[0]))
+                        {
+                            int oldDigitSymbol = int.Parse(item[0].ToString());
+                            var newDigitSymbol = (9 - oldDigitSymbol).ToString();
+                            sb.Append(newDigitSymbol);
+                        }
+                        
+                        for (int i = 1; i < length; i++)
+                        {
+                            if (i % 4 == 0 && i != 16)
+                            {
+                                sb.Append('-');
+                            }
+
+                            if (char.IsLetter(item[i]))
+                            {
+                                var newSymbol = item[i].ToString().ToUpper();
+                                sb.Append(newSymbol);
+                            }
+                            if (char.IsDigit(item[i]))
+                            {
+                                int oldDigitSymbol = int.Parse(item[i].ToString());
+                                var newDigitSymbol = (9 - oldDigitSymbol).ToString();
+                                sb.Append(newDigitSymbol);
+                            }
+                        }
+
+                        list.Add(sb.ToString());
+                    }
+
+                    if (length == 25)
+                    {
+                        var sb = new StringBuilder();
+
+                        if (char.IsLetter(item[0]))
+                        {
+                            var newSymbol = item[0].ToString().ToUpper();
+                            sb.Append(newSymbol);
+                        }
+                        if (char.IsDigit(item[0]))
+                        {
+                            int oldDigitSymbol = int.Parse(item[0].ToString());
+                            var newDigitSymbol = (9 - oldDigitSymbol).ToString();
+                            sb.Append(newDigitSymbol);
+                        }
+
+                        for (int i = 1; i < length; i++)
+                        {
+                            if (i % 5 == 0 && i != 25)
+                            {
+                                sb.Append('-');
+                            }
+
+                            if (char.IsLetter(item[i]))
+                            {
+                                var newSymbol = item[i].ToString().ToUpper();
+                                sb.Append(newSymbol);
+                            }
+                            if (char.IsDigit(item[i]))
+                            {
+                                int oldDigitSymbol = int.Parse(item[i].ToString());
+                                var newDigitSymbol = (9 - oldDigitSymbol).ToString();
+                                sb.Append(newDigitSymbol);
+                            }
+                        }
+
+                        list.Add(sb.ToString());
+                    }
+                }
+            }
+
+            Console.WriteLine($"{string.Join(", ", list)}");
+        }
+    }
+}
