0f989d33b1cab65c72d959e173b303b6b31adc8f
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-07-31 22:30:34 +0300

    add "Final Exam 06-April-2019"

---
+++ CSharp Fundamentals/Exams/Fundamentals Final Exam - 06 April 2019/01. Dictionary/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wodrsAndDefinitions = Console.ReadLine().Split(" | ");
+
+            var dict = new Dictionary<string, List<string>>();
+
+            foreach (var item in wodrsAndDefinitions)
+            {
+                var splitedItems = item.Split(": ");
+
+                var word = splitedItems[0];
+                var definition = splitedItems[1];
+
+                if (!dict.ContainsKey(word))
+                {
+                    dict[word] = new List<string>();
+                }
+
+                dict[word].Add(definition);
+            }
+
+            var inputCheckers = Console.ReadLine().Split(" | ");
+
+            foreach (var checker in inputCheckers)
+            {
+                if (dict.ContainsKey(checker))
+                {
+                    Console.WriteLine($"{checker}");
+
+                    foreach (var item in dict[checker].OrderByDescending(x => x.Length))
+                    {
+                        Console.WriteLine($" -{item}");
+                    }
+                }
+            }
+
+            var command = Console.ReadLine();
+
+            if (command == "End")
+            {
+                return;
+            }
+            else if (command == "List")
+            {
+                Console.WriteLine(string.Join(" ", dict.Keys.OrderBy(x => x)));
+
+            }
+        }
+    }
+}
---
+++ CSharp Fundamentals/Exams/Fundamentals Final Exam - 06 April 2019/02. Deciphering/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Deciphering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+
+            var pattern = @"^[d-z{}|#]+$";
+            var match = Regex.Match(text, pattern);
+
+            if (match.Success)
+            {
+                var substringsInput = Console.ReadLine().Split(" ");
+                var substring = substringsInput[0];
+                var newString = substringsInput[1];
+
+                var sb = new StringBuilder();
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    var ChangedSymbol = (char)(text[i] - 3);
+                    sb.Append(ChangedSymbol);
+                }
+
+                var changedText  = sb.ToString().Replace(substring, newString);
+                Console.WriteLine(changedText);
+
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+        }
+    }
+}
