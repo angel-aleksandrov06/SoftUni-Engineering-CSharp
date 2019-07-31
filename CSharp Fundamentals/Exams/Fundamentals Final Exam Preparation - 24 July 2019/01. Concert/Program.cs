59fc5576d7d50442c595098c48c07b1174df1d5a
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-07-31 22:31:13 +0300

    add "Final Exam Prep 24-July-2019"

---
+++ CSharp Fundamentals/Exams/Fundamentals Final Exam Preparation - 24 July 2019/01. Concert/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace _01._Concert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var dict = new Dictionary<string, List<string>>();
+            var timeGroup = new Dictionary<string, int>();
+
+            while (input != "start of concert")
+            {
+                var splitedInput = input.Split("; ");
+
+                var command = splitedInput[0];
+                var bandName = splitedInput[1];
+                var leftInput = splitedInput[2];
+
+                if (command == "Add")
+                {
+                    var nameOfPlayrs = leftInput.Split(", ");
+
+                    if (!dict.ContainsKey(bandName))
+                    {
+                        dict[bandName] = new List<string>();
+                    }
+
+                    foreach (var item in nameOfPlayrs)
+                    {
+                        if (!dict[bandName].Contains(item))
+                        {
+                            dict[bandName].Add(item);
+                        }
+                    }
+                }
+                else if (command == "Play")
+                {
+                    var time = int.Parse(leftInput);
+
+                    if (!dict.ContainsKey(bandName))
+                    {
+                        dict[bandName] = new List<string>();
+                    }
+
+                    if (!timeGroup.ContainsKey(bandName))
+                    {
+                        timeGroup[bandName] = 0;
+                    }
+
+                    timeGroup[bandName] += time;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var finalInput = Console.ReadLine();
+
+            var totalTime = timeGroup.Values.Sum();
+            Console.WriteLine($"Total time: {totalTime}");
+
+            var orderedByTime = timeGroup.OrderByDescending(x => x.Value).ThenBy(x=>x.Key);
+
+            foreach (var item in orderedByTime)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+            Console.WriteLine($"{finalInput}");
+
+            foreach (var item in dict[finalInput])
+            {
+                Console.WriteLine($"=> {item}");
+            }
+        }
+    }
+}
---
+++ CSharp Fundamentals/Exams/Fundamentals Final Exam Preparation - 24 July 2019/02. Song Encryption/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace _02._Song_Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                var splitedInput = input.Split(":");
+                var artis = splitedInput[0];
+                var song = splitedInput[1];
+
+                var isArtistValid = true;
+                var isSongValid = true;
+
+                if (Char.IsUpper(artis[0]))
+                {
+                    for (int i = 1; i < artis.Length; i++)
+                    {
+                        if (!char.IsLower(artis[i]) && artis[i] != '\'' && artis[i] != ' ')
+                        {
+                            isArtistValid = false;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    isArtistValid = false;
+                }
+
+                if (isArtistValid)
+                {
+                    for (int i = 0; i < song.Length; i++)
+                    {
+                        if (!Char.IsUpper(song[i]) && song[i] != ' ')
+                        {
+                            isSongValid = false;
+                        }
+                    }
+                }
+
+                if (isArtistValid && isSongValid)
+                {
+                    var key = artis.Length;
+
+                    var sb = new StringBuilder();
+
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (input[i] == ' ' || input[i] == '\'')
+                        {
+                            sb.Append(input[i].ToString());
+                        }
+                        else if (input[i] == ':')
+                        {
+                            sb.Append("@");
+                        }
+                        else
+                        {
+                            var newSymbol = (char)(input[i] + key);
+
+                            if (char.IsUpper(input[i]) && newSymbol > 'Z')
+                            {
+                                newSymbol = (char)(newSymbol - 26);
+                            }
+                            else if (char.IsLower(input[i]) && newSymbol > 'z')
+                            {
+                                newSymbol = (char)(newSymbol - 26);
+                            }
+                            sb.Append(newSymbol);
+                        }
+                    }
+                    Console.WriteLine($"Successful encryption: {sb}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
