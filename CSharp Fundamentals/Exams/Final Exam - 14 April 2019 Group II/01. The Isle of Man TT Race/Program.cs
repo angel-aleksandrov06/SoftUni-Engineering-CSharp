c300949f7bfef6ccdaf6ffbde0a4fa4aa4a3eacb
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-08-01 18:11:56 +0300

    add "Final Exam - 14-April-2019"

---
+++ CSharp Fundamentals/Exams/Final Exam - 14 April 2019 Group II/01. The Isle of Man TT Race/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace _03._The_Isle_of_Man_TT_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"([#$%*&])(?<name>[A-Za-z]+)\1=(?<length>[0-9]+)!!(.*)$";
+
+            var coordinatesFound = false;
+
+            while (!coordinatesFound)
+            {
+                var input = Console.ReadLine();
+
+                bool isMatched = Regex.IsMatch(input, pattern);
+
+                if (isMatched)
+                {
+                    var match = Regex.Match(input, pattern);
+                    var length = int.Parse(match.Groups["length"].ToString());
+
+                    int index = int.Parse(input.IndexOf("!!").ToString());
+
+                    var leftSymbols = input.Substring(index + 2);
+
+                    if (leftSymbols.Length == length)
+                    {
+                        var name = match.Groups["name"].ToString();
+                        var hashcode = leftSymbols;
+
+                        var sb = new StringBuilder();
+
+                        for (int i = 0; i < hashcode.Length; i++)
+                        {
+                            var result = (char)(hashcode[i] + length);
+                            sb.Append(result);
+                        }
+
+                        Console.WriteLine($"Coordinates found! {name} -> {sb}");
+
+                        coordinatesFound = true;
+                    }
+                }
+
+                if (!coordinatesFound)
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+            }
+        }
+    }
+}
---
+++ CSharp Fundamentals/Exams/Final Exam - 14 April 2019 Group II/02. Practice Sessions/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02._Practice_Sessions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var dict = new SortedDictionary<string, List<string>>();
+
+            while (input != "END")
+            {
+                var splitedInput = input.Split("->");
+                var command = splitedInput[0];
+
+
+                if (command == "Add")
+                {
+                    var road = splitedInput[1];
+                    var racer = splitedInput[2];
+
+                    if (!dict.ContainsKey(road))
+                    {
+                        dict[road] = new List<string>();
+                    }
+
+                    dict[road].Add(racer);
+                }
+                else if (command == "Move")
+                {
+                    var currentRoad = splitedInput[1];
+                    var racer = splitedInput[2];
+                    var nextRoad = splitedInput[3];
+
+                    if (dict[currentRoad].Contains(racer))
+                    {
+                        dict[nextRoad].Add(racer);
+                        dict[currentRoad].Remove(racer);
+                    }
+
+                }
+                else if (command == "Close")
+                {
+                    var road = splitedInput[1];
+
+                    if (dict.ContainsKey(road))
+                    {
+                        dict.Remove(road);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Practice sessions:");
+            foreach (var road in dict.OrderByDescending(x=>x.Value.Count))
+            {
+                Console.WriteLine($"{road.Key}");
+
+                foreach (var racer in road.Value)
+                {
+                    Console.WriteLine($"++{racer}");
+                }
+            }
+        }
+    }
+}
