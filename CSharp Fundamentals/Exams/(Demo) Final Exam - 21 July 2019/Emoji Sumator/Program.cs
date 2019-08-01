f9f2fd1242fd5c5ed3f145b7c3162dbf96df1f3c
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-08-01 20:53:53 +0300

    add some Exercise to "Final Exam - 21-July-2019"

---
+++ CSharp Fundamentals/Exams/(Demo) Final Exam - 21 July 2019/Emoji Sumator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+namespace Emoji_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"^(?<emoji>:(?<emojiValue>[a-z]{4,}):)$";
+            var text = Console.ReadLine().Split(" ");
+            var inputEmojis = Console.ReadLine().Split(":").ToList();
+
+            var list = new List<string>();
+            var clearNamesOfEmojies = new List<string>();
+            int totalSum = 0;
+
+            foreach (var currentEmoji in text)
+            {
+                if (currentEmoji.Contains(",") || currentEmoji.Contains("!") || currentEmoji.Contains("?") || currentEmoji.Contains("."))
+                {
+                    pattern = @"^(?<emoji>:(?<emojiValue>[a-z]{4,}):)(,|\.|!|\?)$";
+                }
+
+                Match match = Regex.Match(currentEmoji, pattern);
+
+                if (match.Success)
+                {
+                    string valueOfEmoji = match.Groups["emoji"].Value;
+
+                    foreach (var symbol in valueOfEmoji)
+                    {
+                        if (symbol != ':')
+                        {
+                            totalSum += symbol;
+                        }
+                    }
+
+                    clearNamesOfEmojies.Add(valueOfEmoji);
+                }
+
+                pattern = @"^(?<emoji>:(?<emojiValue>[a-z]{4,}):)$";
+            }
+
+            string emojiNameFromInput = ":";
+            foreach (var item in inputEmojis)
+            {
+                char name = (char)int.Parse(item);
+                emojiNameFromInput += name;
+            }
+            emojiNameFromInput += ":";
+
+            if (clearNamesOfEmojies.Contains(emojiNameFromInput))
+            {
+                totalSum *= 2;
+            }
+
+            if (clearNamesOfEmojies.Any())
+            {
+                Console.WriteLine($"Emojis found: {string.Join(", ", clearNamesOfEmojies)}");
+            }
+
+            Console.WriteLine($"Total Emoji Power: {totalSum}");
+        }
+    }
+}
---
+++ CSharp Fundamentals/Exams/(Demo) Final Exam - 21 July 2019/String Manipulator/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace String_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = string.Empty;
+            var input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                var splitedInput = input.Split(" ");
+                var command = splitedInput[0];
+
+                if (command == "Add")
+                {
+                    var newString = splitedInput[1];
+                    str = str + newString;
+                }
+                else if (command == "Upgrade")
+                {
+                    var symbol = char.Parse(splitedInput[1]);
+                    var sb = new StringBuilder();
+
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        if (str[i] == symbol)
+                        {
+                            sb.Append((char)(str[i] + 1));
+                        }
+                        else
+                        {
+                            sb.Append(str[i]);
+                        }
+                    }
+                    str = sb.ToString();
+                }
+                else if (command == "Print")
+                {
+                    Console.WriteLine(str);
+                }
+                else if (command == "Index")
+                {
+                    var symbol = char.Parse(splitedInput[1]);
+                    var sb = new StringBuilder();
+                    List<string> charList = new List<string>();
+                    var index = -1;
+
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        index = str.IndexOf(symbol, index+1);
+                        if (index == -1)
+                        {
+                            break;
+                        }
+                        charList.Add(index.ToString());
+                    }
+
+
+                    if (charList.Count() != 0)
+                    {
+                        Console.WriteLine(string.Join(" ", charList));
+                    }
+                    else
+                    {
+                        Console.WriteLine("None");
+                    }
+                }
+                else if (command == "Remove")
+                {
+                    var del = splitedInput[1];
+                    str = str.Replace(del, string.Empty);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
