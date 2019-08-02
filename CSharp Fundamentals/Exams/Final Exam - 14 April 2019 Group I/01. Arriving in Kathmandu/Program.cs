722fd4aac2d3f7e3cc0f485dbb0e1f6882bde0e7
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-08-02 18:49:21 +0300

    add "Final Exam - 14 April 2019 Group I"

---
+++ CSharp Fundamentals/Exams/Final Exam - 14 April 2019 Group I/01. Arriving in Kathmandu/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace _01._Arriving_in_Kathmandu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var pattern = @"^(?<peak>[A-Za-z0-9!@#$?]+)=(?<length>\d+)<<(?<hash>(.*?)+)$";
+
+            while (input != "Last note")
+            {
+                var match = Regex.Match(input, pattern);
+                var isLengthValid = false;
+
+                if (match.Success)
+                {
+                    var nameofMountain = match.Groups["peak"].ToString();
+                    var length = int.Parse(match.Groups["length"].ToString());
+                    var hash = match.Groups["hash"].ToString();
+
+                    if (length == hash.Length)
+                    {
+                        isLengthValid = true;
+
+                        var sb = new StringBuilder();
+
+                        for (int i = 0; i < nameofMountain.Length; i++)
+                        {
+                            if (char.IsLetterOrDigit(nameofMountain[i]))
+                            {
+                                sb.Append(nameofMountain[i]);
+                            }
+                        }
+                        nameofMountain = sb.ToString();
+
+                        Console.WriteLine($"Coordinates found! {nameofMountain} -> {hash}");
+                    }
+                }
+                if (isLengthValid == false)
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ CSharp Fundamentals/Exams/Final Exam - 14 April 2019 Group I/02. On the Way to Annapurna/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._On_the_Way_to_Annapurna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var dict = new Dictionary<string, List<string>>();
+
+            while (input != "END")
+            {
+                var splitedInput = input.Split("->");
+                var command = splitedInput[0];
+                var store = splitedInput[1];
+
+                if (command == "Add")
+                {
+                    var items = splitedInput[2].Split(",");
+
+                    if (!dict.ContainsKey(store))
+                    {
+                        dict[store] = new List<string>();
+                    }
+
+                    foreach (var item in items)
+                    {
+                        dict[store].Add(item);
+                    }
+                }
+                else if (command == "Remove")
+                {
+                    if (dict.ContainsKey(store))
+                    {
+                        dict.Remove(store);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Stores list:");
+
+            foreach (var item in dict.OrderByDescending(x=>x.Value.Count).ThenByDescending(x=>x.Key))
+            {
+                Console.WriteLine($"{item.Key}");
+                foreach (var thinks in item.Value)
+                {
+                    Console.WriteLine($"<<{thinks}>>");
+                }
+            }
+        }
+    }
+}
