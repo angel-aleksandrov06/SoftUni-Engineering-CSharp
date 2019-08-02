14c5a89eb8193f8386c4624d344b96ffaa3b033a
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-08-02 17:29:09 +0300

    add "Final Exam - 18-April-2018"

---
+++ CSharp Fundamentals/Exams/Retake Final Exam - 18 April 2018/01. Animal Sanctuary/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01._Animal_Sanctuary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            int totalSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                var pattern = @"^n:([^;]+);t:([^;]+);c--([A-Za-z' ']+)$";
+
+                var match = Regex.Match(input, pattern);
+
+                if (match.Success)
+                {
+                    var message = match.ToString().Split(";");
+
+                    var animalName = message[0];
+                    var animalKind = message[1];
+                    var country = message[2].Substring(3);
+
+                    var sb = new StringBuilder();
+
+                    for (int j = 0; j < animalName.Length; j++)
+                    {
+                        if (char.IsLetter(animalName[j]) || animalName[j] == ' ')
+                        {
+                            sb.Append(animalName[j]);
+                        }
+
+                        if (char.IsDigit(animalName[j]))
+                        {
+                            totalSum += int.Parse(animalName[j].ToString());
+                        }
+                    }
+
+                    animalName = sb.ToString();
+                    animalName = animalName.Substring(1);
+                    sb.Clear();
+
+                    for (int j = 0; j < animalKind.Length; j++)
+                    {
+                        if (char.IsLetter(animalKind[j]) || animalKind[j] == ' ')
+                        {
+                            sb.Append(animalKind[j]);
+                        }
+                        if (char.IsDigit(animalKind[j]))
+                        {
+                            totalSum += int.Parse(animalKind[j].ToString());
+                        }
+                    }
+
+                    animalKind = sb.ToString();
+                    animalKind = animalKind.Substring(1);
+
+                    Console.WriteLine($"{animalName} is a {animalKind} from {country}");
+
+                }
+            }
+            Console.WriteLine($"Total weight of animals: {totalSum}KG");
+        }
+    }
+}
---
+++ CSharp Fundamentals/Exams/Retake Final Exam - 18 April 2018/02. Feed the Animals/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Feed_the_Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var dictAnimalsFood = new Dictionary<string, int>();
+            var dictAreas = new Dictionary<string, List<string>>();
+
+            while (input != "Last Info")
+            {
+                var splitedInput = input.Split(":");
+                var command = splitedInput[0];
+
+                if (command == "Add")
+                {
+                    var animalName = splitedInput[1];
+                    var daylyFoodLimit = int.Parse(splitedInput[2]);
+                    var area = splitedInput[3];
+
+                    if (!dictAnimalsFood.ContainsKey(animalName))
+                    {
+                        dictAnimalsFood[animalName] = 0;
+                    }
+
+                    dictAnimalsFood[animalName] += daylyFoodLimit;
+
+                    if (!dictAreas.ContainsKey(area))
+                    {
+                        dictAreas[area] = new List<string>();
+                    }
+                    if (!dictAreas[area].Contains(animalName))
+                    {
+                        dictAreas[area].Add(animalName);
+                    }
+                }
+                else if (command == "Feed")
+                {
+                    var animalName = splitedInput[1];
+                    var food = int.Parse(splitedInput[2]);
+                    var area = splitedInput[3];
+
+                    if (dictAnimalsFood.ContainsKey(animalName))
+                    {
+                        dictAnimalsFood[animalName] -= food;
+                        if (dictAnimalsFood[animalName] <= 0)
+                        {
+                            Console.WriteLine($"{animalName} was successfully fed");
+                            dictAnimalsFood.Remove(animalName);
+                            dictAreas[area].Remove(animalName);
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+
+            Console.WriteLine("Animals:");
+            foreach (var item in dictAnimalsFood.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}g");
+            }
+
+            Console.WriteLine("Areas with hungry animals:");
+            foreach (var item in dictAreas.OrderByDescending(x => x.Value.Count).Where(x => x.Value.Count > 0))
+            {
+                Console.WriteLine($"{item.Key} : {item.Value.Count}");
+            }
+        }
+    }
+}
