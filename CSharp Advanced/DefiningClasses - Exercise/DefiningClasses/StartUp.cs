5266bb7bba54bc33819bbae5cfe3d8dfcc422351
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-10-11 17:48:09 +0300

    add Defining Classes - Exercise

---
+++ CSharp Advanced/DefiningClasses - Exercise/DefiningClasses/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Family family = new Family();
+
+            var count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                var personInfo = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                var name = personInfo[0];
+                var age = int.Parse(personInfo[1]);
+
+                Person person = new Person(name, age);
+
+                family.AddMember(person);
+            }
+
+            var oldestMember = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+
+
+}
