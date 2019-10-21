46aa13f821d7686a4fcab93889a317dd9925604c
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-10-21 15:33:28 +0300

    add Generics-Exercise 01, 02

---
+++ CSharp Advanced/Generics - Exercise/01. GenericBoxOfString/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Generics
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            var inputCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                var input = Console.ReadLine();
+
+                var box = new Box<string>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/Generics - Exercise/02. GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Generics
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            var inputCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+
+                var box = new Box<int>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
