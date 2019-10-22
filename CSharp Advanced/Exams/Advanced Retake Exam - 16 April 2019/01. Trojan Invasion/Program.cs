fedff3f66b15c400b7dda24d634551b22b6c2205
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-10-22 22:01:01 +0300

    add Advanced Exam 16 April ( Exercise 01 )

---
+++ CSharp Advanced/Exams/Advanced Retake Exam - 16 April 2019/01. Trojan Invasion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Trojan_Invasion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfTrojanWaves = int.Parse(Console.ReadLine());
+            var platesOfSpartanDefense = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            for (int i = 0; i < numberOfTrojanWaves; i++)
+            {
+                var powerOfEachTrojanWarrior = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+                while (true)
+                {
+                    if (powerOfEachTrojanWarrior.Count == 0 || platesOfSpartanDefense.Count == 0)
+                    {
+                        break;
+                    }
+
+                    var trojanWarrior = powerOfEachTrojanWarrior.Pop();
+                    var spartanPlate = platesOfSpartanDefense.Dequeue();
+
+                    var attack = trojanWarrior - spartanPlate;
+
+                    if (attack < 0)
+                    {
+                        spartanPlate = Math.Abs(attack);
+                        platesOfSpartanDefense.Enqueue
+                    }
+                }
+            }
+
+        }
+    }
+}
