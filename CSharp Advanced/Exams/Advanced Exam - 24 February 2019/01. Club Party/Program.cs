ddf1afc3b3a3de14926216675cccfecd2750e3ff
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-10-24 22:48:56 +0300

    add Exam 24 February 2019 ( 03 ) Exercise

---
+++ CSharp Advanced/Exams/Advanced Exam - 24 February 2019/01. Club Party/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Club_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ CSharp Advanced/Exams/Advanced Exam - 24 February 2019/03. Heroes/HeroRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes
+{
+    public class HeroRepository
+    {
+        private List<Hero> heroes;
+        public HeroRepository()
+        {
+            this.heroes = new List<Hero>();
+        }
+
+        public int Count => heroes.Count;
+
+        public void Add(Hero hero)
+        {
+            heroes.Add(hero);
+        }
+
+        public void Remove(string name)
+        {
+            if (heroes.Any(x => x.Name == name))
+            {
+                heroes.Remove(heroes.Find(x => x.Name == name));
+            }
+        }
+
+        public Hero GetHeroWithHighestStrength()
+        {
+            Hero hero = heroes[0];
+
+            foreach (var item in heroes)
+            {
+                if (item.Item.Strength > hero.Item.Strength)
+                {
+                    hero = item;
+                }
+            }
+            return hero;
+        }
+
+        public Hero GetHeroWithHighestAbility()
+        {
+            Hero hero = heroes[0];
+
+            foreach (var item in heroes)
+            {
+                if (item.Item.Ability > hero.Item.Ability)
+                {
+                    hero = item;
+                }
+            }
+            return hero;
+        }
+
+        public Hero GetHeroWithHighestIntelligence()
+        {
+            Hero hero = heroes[0];
+
+            foreach (var item in heroes)
+            {
+                if (item.Item.Intelligence > hero.Item.Intelligence)
+                {
+                    hero = item;
+                }
+            }
+            return hero;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var hero in heroes)
+            {
+                sb.AppendLine(hero.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
