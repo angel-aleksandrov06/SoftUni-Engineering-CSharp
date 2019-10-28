f46001494ac161197603d430cac08595036bbf4b
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-10-28 18:05:25 +0200

    add Iterators and Comparators - Exercise 03, 04, 05

---
+++ CSharp Advanced/Iterators and Comparators - Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.ComparingObjects
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var people = new List<Person>();
+
+            var command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                var personInfo = command.Split().ToArray();
+
+                var name = personInfo[0];
+                var age = int.Parse(personInfo[1]);
+                var town = personInfo[2];
+
+                var person = new Person(name, age, town);
+
+                people.Add(person);
+
+                command = Console.ReadLine();
+            }
+
+            var n = int.Parse(Console.ReadLine());
+
+            var targetPerson = people[n - 1];
+
+            var matches = 1;
+
+            foreach (var person in people)
+            {
+                if (person.CompareTo(targetPerson) == 0 && !person.Equals(targetPerson))
+                {
+                    matches++;
+                }
+            }
+
+            if (matches == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{matches} {people.Count-matches} {people.Count}");
+            }
+        }
+    }
+}
