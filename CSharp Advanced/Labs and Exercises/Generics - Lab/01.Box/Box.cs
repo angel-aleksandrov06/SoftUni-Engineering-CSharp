4697ffabfc259a4b6358291d8fd9b84a80ce8bf7
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-10-29 15:36:05 +0200

    add Generics - Lab "all exercises"

---
+++ CSharp Advanced/Labs and Exercises/Generics - Lab/01.Box/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> things;
+        public Box()
+        {
+            this.things = new List<T>();
+        }
+
+        public int Count => things.Count;
+
+        public void Add(T element)
+        {
+            things.Add(element);
+        }
+        public T Remove()
+        {
+            T element = things.Last();
+
+            this.things.RemoveAt(this.things.Count - 1);
+
+            return element;
+        }
+    }
+}
---
+++ CSharp Advanced/Labs and Exercises/Generics - Lab/01.Box/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
