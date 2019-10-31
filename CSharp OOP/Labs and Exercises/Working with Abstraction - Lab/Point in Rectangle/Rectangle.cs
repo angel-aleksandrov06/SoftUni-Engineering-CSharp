50ff3112e43ced1ccede284e6a2d5d37869ca540
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-10-31 13:43:21 +0200

    add Working with Abstraction - Lab ( 03 exercise )

---
+++ CSharp OOP/Labs and Exercises/Working with Abstraction - Lab/Point in Rectangle/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿namespace Point_in_Rectangle
+{
+    public class Rectangle
+    {
+        public Rectangle(Point x, Point y)
+        {
+            this.TopLeft = x;
+            this.BottomRight = y;
+        }
+
+        public Point TopLeft { get; set; }
+
+        public Point BottomRight { get; set; }
+
+        public bool Contains(Point point)
+        {
+            bool isInHorizontal = this.TopLeft.X <= point.X && this.BottomRight.X >= point.X;
+            bool isInVertical = this.TopLeft.Y <= point.Y && this.BottomRight.Y >= point.Y;
+
+            bool isInRectangle = isInHorizontal && isInVertical;
+
+            return isInRectangle;
+        }
+    }
+}
---
+++ CSharp OOP/Labs and Exercises/Working with Abstraction - Lab/Point in Rectangle/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Point_in_Rectangle
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var rangeOfREgtangle = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var topLeftX = rangeOfREgtangle[0];
+            var topLeftY = rangeOfREgtangle[1];
+            var bottomRightX = rangeOfREgtangle[2];
+            var bottomRightY = rangeOfREgtangle[3];
+
+            Point topLeft = new Point(topLeftX, topLeftY);
+            Point bottomRight = new Point(bottomRightX, bottomRightY);
+
+            Rectangle rectangle = new Rectangle(topLeft, bottomRight);
+
+            var inputCounts = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputCounts; i++)
+            {
+                var inputCurretPoint = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                var inputX = inputCurretPoint[0];
+                var inputY = inputCurretPoint[1];
+
+                var currentPoint = new Point(inputX, inputY);
+
+                var isInRange = rectangle.Contains(currentPoint);
+                Console.WriteLine(isInRange);
+            }
+        }
+    }
+}
