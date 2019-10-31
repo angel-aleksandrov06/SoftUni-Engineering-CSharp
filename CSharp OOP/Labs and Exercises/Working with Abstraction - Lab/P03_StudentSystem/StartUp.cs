de29601f7605114631056510ce1e794b2dddc8b0
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-10-31 13:43:11 +0200

    add Working with Abstraction - Lab ( 03 exercise )

---
+++ CSharp OOP/Labs and Exercises/Working with Abstraction - Lab/P03_StudentSystem/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace P03_StudentSystem
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            StudentSystem studentSystem = new StudentSystem();
+            studentSystem.ParseCommands();
+        }
+    }
+}
---
+++ CSharp OOP/Labs and Exercises/Working with Abstraction - Lab/P03_StudentSystem/StudentSystem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace P03_StudentSystem
+{
+    public class StudentSystem
+    {
+        private StudentsDatabase students;
+        public StudentSystem()
+        {
+            this.students = new StudentsDatabase();
+        }
+
+        public void ParseCommands()
+        {
+            while (true)
+            {
+                string[] args = Console.ReadLine().Split();
+                var commandName = args[0];
+                if (commandName == "Create")
+                {
+                    this.Create(args);
+                }
+                else if (commandName == "Show")
+                {
+                    this.Show(args);
+                }
+                else if (commandName == "Exit")
+                {
+                    return;
+                }
+            }
+        }
+
+        private void Create(string[] args)
+        {
+            var name = args[1];
+            var age = int.Parse(args[2]);
+            var grade = double.Parse(args[3]);
+            this.students.Add(name, age, grade);
+        }
+
+        private void Show(string[] args)
+        {
+            var name = args[1];
+            var student = this.students.Find(name);
+            if (student != null)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
