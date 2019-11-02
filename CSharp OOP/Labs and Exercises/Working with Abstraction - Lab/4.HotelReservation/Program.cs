d761c584670319f98a5a4a752ede3a064e9b9fcc
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-11-02 22:59:29 +0200

    add Working with Abstraction - Exercise "ressources"

---
+++ CSharp OOP/Labs and Exercises/Working with Abstraction - Lab/4.HotelReservation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4.HotelReservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
