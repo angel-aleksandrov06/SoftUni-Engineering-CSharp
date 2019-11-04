03e9c6a226482075667c1bb0a21094b5ad30397b
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-11-05 00:26:30 +0200

    Refactoring Lab- 04

--- CSharp OOP/Labs and Exercises/Working with Abstraction - Lab/4.HotelReservation/Program.cs
+++ CSharp OOP/Labs and Exercises/Working with Abstraction - Lab/4.HotelReservation/Program.cs
@@ -6,7 +6,21 @@ namespace _4.HotelReservation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            decimal pricePerDay = decimal.Parse(input[0]);
+            int numberOfDays = int.Parse(input[1]);
+            string season = input[2].ToLower();
+            string discountType = "none";
+
+            if (input.Length > 3) discountType = input[3].ToLower();
+
+            decimal totalPrice = PriceCalculator.CalculatePrice(pricePerDay,
+                numberOfDays,
+                Enum.Parse<Season>(season),
+                Enum.Parse<Discount>(discountType));
+
+            Console.WriteLine($"{totalPrice:f2}");
         }
     }
 }
