8eedbec76ef41e089eeb61b3f2ce2e1d9dae65fa
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-11-24 15:52:36 +0200

    add "Exercise 01CommandPattern"

---
+++ CSharp OOP/Labs and Exercises/Reflection-and-Attributes - Exercise/CommandPattern/Core/Commands/OpenCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace CommandPattern.Core.Commands
+{
+    using Contracts;
+
+    public class OpenCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            string path = args[0];
+
+            System.Diagnostics.Process.Start("cmd", $"/c start {path}");
+
+            return "Started successfully!";
+        }
+    }
+}
--- CSharp OOP/Labs and Exercises/Reflection-and-Attributes - Exercise/CommandPattern/StartUp.cs
+++ CSharp OOP/Labs and Exercises/Reflection-and-Attributes - Exercise/CommandPattern/StartUp.cs
@@ -1,14 +1,15 @@
-﻿using System;
-
-namespace CommandPattern
+﻿namespace CommandPattern
 {
+    using Core;
+    using Core.Contracts;
+
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            //ICommandInterpreter command = new CommandInterpreter();
-            //IEngine engine = new Engine(command);
-            //engine.Run();
+            ICommandInterpreter command = new CommandInterpreter();
+            IEngine engine = new Engine(command);
+            engine.Run();
         }
     }
 }
