32cabe70eab7281f4e43fddc96d2e566a004b9d8
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-11-26 23:52:49 +0200

    add Inheritance Lab "02MultipleInheritance"

---
+++ CSharp OOP/Labs and Exercises/Inheritance - Lab/Inheritance - Lab/02MultipleInheritance/Puppy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Farm;
+
+namespace Farm
+{
+    public class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+}
