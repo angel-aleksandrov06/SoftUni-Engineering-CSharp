4f60c056a62cf64d943ed6f9d93c6b2f2e0149f9
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-11-28 00:22:31 +0200

    Add Polymorphism - Lab { 01MathOperations, 02Animals, 03Shapes } Done

---
+++ CSharp OOP/Labs and Exercises/Polymorphism - Lab/Shapes/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Math.PI * 2 * this.Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
