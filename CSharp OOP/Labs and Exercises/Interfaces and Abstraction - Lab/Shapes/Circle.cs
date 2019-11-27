1f7b6656dd7943f7522b8b319d7a066199cff1a7
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2019-11-27 16:29:39 +0200

    add Interfaces and Abstraction - Lab { 01Shapes }

---
+++ CSharp OOP/Labs and Exercises/Interfaces and Abstraction - Lab/Shapes/Circle.cs
@@ -0,0 +1,39 @@
+﻿namespace Shapes
+{
+    using System;
+
+    public class Circle : IDrawable
+    {
+        private const double range = 0.4;
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public void Draw()
+        {
+            double rIn = this.radius - range;
+            double rOut = this.radius + range;
+
+            for (double y = this.radius; y >= -this.radius; --y)
+            {
+                for (double x = -this.radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+
+                    if (value >= rIn*rIn && value <= rOut*rOut)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
