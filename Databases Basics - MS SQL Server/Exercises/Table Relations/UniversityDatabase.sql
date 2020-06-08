51ff09644c99fadf23a523f99de9c0f0013d3572
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-06-08 16:52:57 +0300

    Refactoring "Table Relations"
    ManyToManyRelationship,
    SelfReferencing,
    UniversityDatabase

--- Databases Basics - MS SQL Server/Exercises/Table Relations/UniversityDatabase.sql
+++ Databases Basics - MS SQL Server/Exercises/Table Relations/UniversityDatabase.sql
@@ -1,30 +1,34 @@
- CREATE TABLE Majors
- (
-	MajorID INT PRIMARY KEY ,
+CREATE TABLE Majors
+(
+	MajorID INT PRIMARY KEY IDENTITY,
 	[Name] NVARCHAR(50) NOT NULL
- )
- CREATE TABLE Students
- (
+)
+
+CREATE TABLE Students
+(
 	StudentID INT PRIMARY KEY IDENTITY,
 	StudentNumber VARCHAR(10) NOT NULL,
 	StudentName NVARCHAR(50) NOT NULL,
 	MajorID INT NOT NULL FOREIGN KEY REFERENCES Majors(MajorID)
- )
- CREATE TABLE Payments
- (
+)
+
+CREATE TABLE Payments
+(
 	PaymentID INT PRIMARY KEY IDENTITY,
 	PaymentDate DATE NOT NULL,
 	PaymentAmount DECIMAL(15,2) NOT NULL,
 	StudentID INT NOT NULL FOREIGN KEY REFERENCES Students(StudentID)
- )
- CREATE TABLE Subjects
- (
+)
+
+CREATE TABLE Subjects
+(
 	SubjectID INT PRIMARY KEY IDENTITY,
 	SubjectName NVARCHAR(80) NOT NULL
- )
- CREATE TABLE Agenda
- (
+)
+
+CREATE TABLE Agenda
+(
 	StudentID INT NOT NULL FOREIGN KEY REFERENCES Students(StudentID),
 	SubjectID INT NOT NULL FOREIGN KEY REFERENCES Subjects(SubjectID),
 	PRIMARY KEY (StudentID, SubjectID)
- )
+)
