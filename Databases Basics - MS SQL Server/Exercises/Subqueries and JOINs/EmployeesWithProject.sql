ecd4abc90c5de3fb0c4413704b825b96462bf893
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-06-10 15:14:01 +0300

    add four exercises in "Subqueries and JOINs"
    "EmployeesWithoutProjects",
    "EmployeesHiredAfter",
    "EmployeesWithProject",
    "Employee24"

---
+++ Databases Basics - MS SQL Server/Exercises/Subqueries and JOINs/EmployeesWithProject.sql
@@ -0,0 +1,8 @@
+SELECT TOP(5) e.EmployeeID, e.FirstName, p.[Name] AS [ProjectName]
+	FROM Employees e
+	JOIN EmployeesProjects ep
+	ON e.EmployeeID = ep.EmployeeID
+	JOIN Projects p
+	ON p.ProjectID = ep.ProjectID
+	WHERE p.StartDate > '2002.08.13' AND (p.EndDate IS NULL)
+	ORDER BY e.EmployeeID ASC
