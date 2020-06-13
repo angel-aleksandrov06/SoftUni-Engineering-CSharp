62570375f71673271f46481b3c91e70e29b7455f
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-06-13 22:25:54 +0300

    Create SmallestDepositGroupPerMagicWandSize.sql

---
+++ Databases Basics - MS SQL Server/Exercises/Data Aggregation/SmallestDepositGroupPerMagicWandSize.sql
@@ -0,0 +1,6 @@
+SELECT TOP(2) DepositGroup
+	FROM (SELECT DepositGroup, AVG(MagicWandSize) AS [AverageWandSize]
+			FROM WizzardDeposits
+			GROUP BY DepositGroup
+		 ) AS [AverageWandSizeQuery]
+	ORDER BY [AverageWandSize]
