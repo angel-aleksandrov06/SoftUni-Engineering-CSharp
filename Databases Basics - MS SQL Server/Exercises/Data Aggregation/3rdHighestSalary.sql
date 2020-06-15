59fe4af274229a238f3ae267da7c793f09b6e54c
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-06-15 17:59:51 +0300

    Update "EmployeesMinimumSalaries" exercise

--- Databases Basics - MS SQL Server/Exercises/Data Aggregation/3rdHighestSalary.sql
+++ Databases Basics - MS SQL Server/Exercises/Data Aggregation/3rdHighestSalary.sql
@@ -1,2 +1,9 @@
-SELECT *, DENSE_RANK() OVER(PARTITION BY DepartmentID ORDER BY Salary DESC) AS [SalaryRank]
-	FROM Employees
+SELECT DepartmentID, Salary AS [ThirdHighestSalary]
+	FROM (
+			SELECT DepartmentID,
+			       Salary,
+				   DENSE_RANK() OVER(PARTITION BY DepartmentID ORDER BY Salary DESC) AS [SalaryRank]
+			FROM Employees
+			GROUP BY DepartmentID, Salary
+		 ) AS [SalaryRankingsQuery]
+	WHERE SalaryRank = 3
