edbdc582f4b042782285f60ba6d1f7ca93b7289c
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-06-16 21:09:59 +0300

    Update CalculatingInterest.sql

--- Databases Basics - MS SQL Server/Exercises/Database Programmability and Transactions/CalculatingInterest.sql
+++ Databases Basics - MS SQL Server/Exercises/Database Programmability and Transactions/CalculatingInterest.sql
@@ -1,5 +1,13 @@
-CREATE PROC usp_CalculateFutureValueForAccount (@accountId INT, @interestRate FLOAT)
+CREATE PROC usp_CalculateFutureValueForAccount(@AccountId INT, @yir FLOAT)
 AS
 BEGIN
-	
+	SELECT  a.Id AS [Account Id],
+			ah.FirstName AS [First Name],
+			ah.LastName AS [Last Name],
+			a.Balance AS [Current Balance],
+			dbo.ufn_CalculateFutureValue (a.Balance, @yir, 5) AS [Balance in 5 years]
+		FROM AccountHolders ah
+		JOIN Accounts a 
+		ON a.Id = ah.Id
+		WHERE a.Id = @AccountId
 END
