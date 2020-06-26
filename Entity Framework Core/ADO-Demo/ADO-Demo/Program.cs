fe5785c1ec82a60dfb74f4d954327a9d609177e2
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-06-26 20:35:54 +0300

    add "ADO-Demo"

---
+++ Entity Framework Core/ADO-Demo/ADO-Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace ADO_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection("Server=.;Database=SoftUni;Integrated Security=true"))
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("SELECT [FirstName], LastName FROM [EMPLOYEES] WHERE FirstName LIKE 'N%'", sqlConnection);
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string firstName = (string)reader["FirstName"];
+                    string lastName = (string)reader["LastName"];
+                    Console.WriteLine(firstName + " " + lastName);
+                }
+            }
+        }
+    }
+}
