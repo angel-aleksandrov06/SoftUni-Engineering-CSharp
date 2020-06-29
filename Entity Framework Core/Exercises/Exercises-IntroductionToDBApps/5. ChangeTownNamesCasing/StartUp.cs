dd5d24595929388d4933a43f06f142fee5622b34
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-06-29 15:47:41 +0300

    Add "2.VillainNames" and "5. ChangeTownNamesCasing"

---
+++ Entity Framework Core/Exercises/Exercises-IntroductionToDBApps/5. ChangeTownNamesCasing/StartUp.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5._ChangeTownNamesCasing
+{
+    public class StartUp
+    {
+        private const string ConnectionString = @"Server=DESKTOP-U1JHA1S;Database=MinionsDB;Integrated Security=true;";
+
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+
+            sqlConnection.Open();
+
+            string givenCountry = Console.ReadLine();
+
+            string result = ChangeTownNames(sqlConnection, givenCountry);
+
+            Console.WriteLine(result);
+        }
+
+        private static string ChangeTownNames(SqlConnection sqlConnection, string givenCountry)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string updateNamesQueryText = @"UPDATE Towns
+                                   SET Name = UPPER(Name)
+                                   WHERE CountryCode = (SELECT c.Id FROM Countries c WHERE c.Name = @countryName)";
+
+            using SqlCommand updateNamesCmd = new SqlCommand(updateNamesQueryText, sqlConnection);
+            updateNamesCmd.Parameters.AddWithValue("@countryName", givenCountry);
+
+            int changesRows = updateNamesCmd.ExecuteNonQuery();
+
+            if (changesRows == 0)
+            {
+                sb.AppendLine("No town names were affected.");
+
+                return sb.ToString().Trim();
+            }
+
+            sb.AppendLine($"{changesRows} town names were affected.");
+
+            string getChangeTownNamesQueryText = @"SELECT t.Name 
+                                                    FROM Towns t
+                                                    JOIN Countries c ON c.Id = t.CountryCode
+                                                    WHERE c.Name = @countryName";
+
+            using SqlCommand getChangeTownNamesCmd = new SqlCommand(getChangeTownNamesQueryText, sqlConnection);
+
+            getChangeTownNamesCmd.Parameters.AddWithValue("@countryName", givenCountry);
+
+            using SqlDataReader reader = getChangeTownNamesCmd.ExecuteReader();
+
+            sb.Append("[");
+
+            var towns = new List<string>();
+
+            while (reader.Read())
+            {
+                string townName = reader["Name"]?.ToString().Trim();
+                towns.Add(townName);
+            }
+
+            sb.AppendJoin(", ", towns);
+            sb.Append("]");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ Entity Framework Core/Exercises/Exercises-IntroductionToDBApps/VillainNames/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Text;
+
+namespace VillainNames
+{
+    public class StartUp
+    {
+        private const string ConnectionString = @"Server=DESKTOP-U1JHA1S;Database=MinionsDB;Integrated Security=true;";
+
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+
+            sqlConnection.Open();
+
+            string result = GetVillainNamesAndCountMinions(sqlConnection);
+
+            Console.WriteLine(result);
+        }
+
+        private static string GetVillainNamesAndCountMinions(SqlConnection sqlConnection)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string GetvillainsAndMinionsCountQueryText = @"SELECT v.[Name], COUNT(mv.MinionId) AS [CountMinions]
+		                                                        FROM Villains v
+		                                                        JOIN MinionsVillains mv
+		                                                        ON v.Id = mv.VillainId
+		                                                        GROUP BY v.Id, v.[Name]
+		                                                        HAVING COUNT(mv.MinionId) > 3
+		                                                        ORDER BY CountMinions DESC";
+
+            using SqlCommand getVillainNamesAndCoutnMinionsCmd = new SqlCommand(GetvillainsAndMinionsCountQueryText, sqlConnection);
+
+            using SqlDataReader reader = getVillainNamesAndCoutnMinionsCmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string villainName = reader["Name"]?.ToString().Trim();
+                string minionsCount = reader["CountMinions"]?.ToString().Trim();
+
+                sb.AppendLine($"{villainName} - {minionsCount}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
