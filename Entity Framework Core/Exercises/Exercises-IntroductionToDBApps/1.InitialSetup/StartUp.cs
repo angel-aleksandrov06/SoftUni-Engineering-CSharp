b5687afa4f00912b6695ed04c81082be95c3465f
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-06-30 21:17:19 +0300

    add exercises { 01, 06, 08 }
    1.InitialSetup,
    6.RemoveVillain,
    8.IncreaseMinionAge

---
+++ Entity Framework Core/Exercises/Exercises-IntroductionToDBApps/1.InitialSetup/StartUp.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _1.InitialSetup
+{
+    public class StartUp
+    {
+        private const string ConnectionStringMaster = @"Server=DESKTOP-U1JHA1S;Database=master;Integrated Security=true;";
+        private const string ConnectionStringMinionsDB = @"Server=DESKTOP-U1JHA1S;Database=MinionsDB;Integrated Security=true;";
+
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnectionMaster = new SqlConnection(ConnectionStringMaster);
+
+            sqlConnectionMaster.Open();
+
+            // Create database MinionsDB
+            string query = @"CREATE DATABASE MinionsDB";
+            using SqlCommand command = new SqlCommand(query, sqlConnectionMaster);
+            command.ExecuteNonQuery();
+
+            Console.WriteLine("Database was created successful!");
+
+            //Conect to MinionsDB
+            using SqlConnection sqlConnection = new SqlConnection(ConnectionStringMinionsDB);
+            sqlConnection.Open();
+
+            //---------------- Create Tables
+            Console.WriteLine(CreateTables(sqlConnection));
+
+            //---------------- Insert into Tables
+            Console.WriteLine(InsertDataInTables(sqlConnection));
+
+        }
+
+        private static string CreateTables(SqlConnection sqlConnection)
+        {
+            string[] queryes = new string[]
+            {
+                @"CREATE TABLE Countries (Id INT PRIMARY KEY IDENTITY,Name VARCHAR(50))",
+                @"CREATE TABLE Towns(Id INT PRIMARY KEY IDENTITY,Name VARCHAR(50), CountryCode INT FOREIGN KEY REFERENCES Countries(Id))",
+                @"CREATE TABLE Minions(Id INT PRIMARY KEY IDENTITY,Name VARCHAR(30), Age INT, TownId INT FOREIGN KEY REFERENCES Towns(Id))",
+                @"CREATE TABLE EvilnessFactors(Id INT PRIMARY KEY IDENTITY, Name VARCHAR(50))",
+                @"CREATE TABLE Villains (Id INT PRIMARY KEY IDENTITY, Name VARCHAR(50), EvilnessFactorId INT FOREIGN KEY REFERENCES EvilnessFactors(Id))",
+                @"CREATE TABLE MinionsVillains (MinionId INT FOREIGN KEY REFERENCES Minions(Id),VillainId INT FOREIGN KEY REFERENCES Villains(Id),CONSTRAINT PK_MinionsVillains PRIMARY KEY (MinionId, VillainId))"
+            };
+
+            foreach (var query in queryes)
+            {
+                using SqlCommand createCurrentTable = new SqlCommand(query, sqlConnection);
+                createCurrentTable.ExecuteNonQuery();
+            };
+
+            return "You have created all tables successfully!";
+        }
+
+        private static string InsertDataInTables(SqlConnection sqlConnection)
+        {
+            string[] queryes = new string[]
+            {
+                @"INSERT INTO Countries ([Name]) VALUES ('Bulgaria'),('England'),('Cyprus'),('Germany'),('Norway')",
+                @"INSERT INTO Towns ([Name], CountryCode) VALUES ('Plovdiv', 1),('Varna', 1),('Burgas', 1),('Sofia', 1),
+                  ('London', 2),('Southampton', 2),('Bath', 2),('Liverpool', 2),('Berlin', 3),('Frankfurt', 3),('Oslo', 4)",
+                @"INSERT INTO Minions (Name,Age, TownId) VALUES('Bob', 42, 3),('Kevin', 1, 1),('Bob ', 32, 6),('Simon', 45, 3),
+                  ('Cathleen', 11, 2),('Carry ', 50, 10),('Becky', 125, 5),('Mars', 21, 1),('Misho', 5, 10),('Zoe', 125, 5),('Json', 21, 1)",
+                @"INSERT INTO EvilnessFactors (Name) VALUES ('Super good'),('Good'),('Bad'), ('Evil'),('Super evil')",
+                @"INSERT INTO Villains (Name, EvilnessFactorId) VALUES ('Gru',2),('Victor',1),('Jilly',3),('Miro',4),('Rosen',5),('Dimityr',1),('Dobromir',2)",
+                @"INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (4,2),(1,1),(5,7),(3,5),(2,6),(11,5),(8,4),(9,7),(7,1),(1,3),(7,3),(5,3),(4,3),(1,2),(2,1),(2,7)"
+            };
+
+            foreach (var query in queryes)
+            {
+                using SqlCommand insertIntoCurrentTable = new SqlCommand(query, sqlConnection);
+                insertIntoCurrentTable.ExecuteNonQuery();
+            };
+
+            return "Insert was succsessful!";
+        }
+    }
+}
---
+++ Entity Framework Core/Exercises/Exercises-IntroductionToDBApps/8.IncreaseMinionAge/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace _8.IncreaseMinionAge
+{
+    public class StartUp
+    {
+        private const string ConnectionString = @"Server=DESKTOP-U1JHA1S;Database=MinionsDB;Integrated Security=true;";
+
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+
+            sqlConnection.Open();
+
+            var minionsIds = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+
+            string result = updateMinionNamesAndAge(sqlConnection, minionsIds);
+
+            Console.WriteLine(result);
+        }
+
+        private static string updateMinionNamesAndAge(SqlConnection sqlConnection, int[] minionsIds)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string increaseQueryText = @" UPDATE Minions
+                                   SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)), Age += 1
+                                    WHERE Id = @Id";
+
+            foreach (var Id in minionsIds)
+            {
+                using SqlCommand increaseCmd = new SqlCommand(increaseQueryText, sqlConnection);
+                increaseCmd.Parameters.AddWithValue("@Id", Id);
+                increaseCmd.ExecuteNonQuery();
+            }
+
+            string GetNewNameAndAgeQueryText = @"SELECT Name, Age FROM Minions";
+
+            using SqlCommand GetNewNameAndAgeCmd = new SqlCommand(GetNewNameAndAgeQueryText, sqlConnection);
+            using SqlDataReader reader = GetNewNameAndAgeCmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string minionName = reader["Name"]?.ToString();
+                string minionAge = reader["Age"]?.ToString();
+                sb.AppendLine($"{minionName} {minionAge}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
