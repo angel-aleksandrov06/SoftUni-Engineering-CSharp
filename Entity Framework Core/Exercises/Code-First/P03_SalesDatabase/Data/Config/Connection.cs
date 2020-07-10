2839dae28c619ba1273ca25b1d57b1e5d8650445
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-07-10 17:05:23 +0300

    Add directory "Code-First" Exercises { 03, 04-05 }
    "3. Sales Database", - 100/100
    "4-5. Migrations" - 100/100

---
+++ Entity Framework Core/Exercises/Code-First/P03_SalesDatabase/Data/Config/Connection.cs
@@ -0,0 +1,7 @@
+﻿namespace P03_SalesDatabase.Data.Config
+{
+    public static class Connection
+    {
+        public static string ConnectionString = @"Server=.;Database=SalesDatabase;Integrated Security=true;";
+    }
+}
---
+++ Entity Framework Core/Exercises/Code-First/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Config;
+using P03_SalesDatabase.Data.Models;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext()
+        {
+
+        }
+
+        public SalesContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Store> Stores { get; set; }
+
+        public DbSet<Sale> Sales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Connection.ConnectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(en =>
+            {
+                en.Property(e => e.Name)
+                .IsUnicode();
+
+                en.Property(e => e.Description)
+                .HasDefaultValue("No Description");
+            });
+
+            modelBuilder.Entity<Customer>(en =>
+            {
+                en.Property(e => e.Name)
+                .IsUnicode();
+
+                en.Property(e => e.Email)
+                .IsUnicode(false);
+
+                en.Property(e => e.CreditCardNumber)
+                .IsUnicode(false);
+
+            });
+
+            modelBuilder.Entity<Store>(en =>
+            {
+                en.Property(e => e.Name)
+                .IsUnicode();
+            });
+
+            modelBuilder.Entity<Sale>(en =>
+            {
+                en.Property(e => e.Date)
+                .HasDefaultValueSql("GetDate()");
+            });
+        }
+    }
+}
