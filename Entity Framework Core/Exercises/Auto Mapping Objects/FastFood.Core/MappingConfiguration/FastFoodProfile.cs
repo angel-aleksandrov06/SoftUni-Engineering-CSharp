d3c9fe728733f1cceb20e4941530bb1b2ca6269a
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-07-15 22:01:10 +0300

    Add "Auto Mapping Objects" "FastFood"

---
+++ Entity Framework Core/Exercises/Auto Mapping Objects/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,37 @@
+﻿namespace FastFood.Core.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Core.ViewModels.Categories;
+    using FastFood.Core.ViewModels.Employees;
+    using FastFood.Models;
+    using System.Runtime.CompilerServices;
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            //Categories
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.CategoryName));
+
+            this.CreateMap<Category, CategoryAllViewModel>();
+
+            // Employees
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x => x.PositionId, y => y.MapFrom(z => z.Id))
+                .ForMember(x => x.PositionName, y => y.MapFrom(z => z.Name));
+
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.Position, y => y.MapFrom(z => z.Position.Name));
+        }
+    }
+}
