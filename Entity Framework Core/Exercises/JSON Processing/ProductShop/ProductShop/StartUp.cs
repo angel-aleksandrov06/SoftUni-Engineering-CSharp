5e41533b9cc9a2c2cfea442d2b5f4cdcba444cec
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-07-23 23:21:47 +0300

    add exercise "ProductShop" in "JSON Processing"
    Exercises { 02, 03, 04, 05 }

--- Entity Framework Core/Exercises/JSON Processing/ProductShop/ProductShop/StartUp.cs
+++ Entity Framework Core/Exercises/JSON Processing/ProductShop/ProductShop/StartUp.cs
@@ -1,18 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Newtonsoft.Json;
-using ProductShop.Data;
-using ProductShop.Models;
-
-namespace ProductShop
+﻿namespace ProductShop
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Newtonsoft.Json;
+    using ProductShop.Data;
+    using ProductShop.Models;
+
     public class StartUp
     {
+        private static string ResultDirectoryPath = "../../../Datasets/Results";
+
         public static void Main(string[] args)
         {
+            ProductShopContext db = new ProductShopContext();
 
+            string json = GetProductsInRange(db);
+
+            if (!Directory.Exists(ResultDirectoryPath))
+            {
+                Directory.CreateDirectory(ResultDirectoryPath);
+            }
+
+            File.WriteAllText(ResultDirectoryPath + "/products-in-range.json", json);
+            Console.WriteLine(json);
+        }
+
+        private static void ResetDatabase(ProductShopContext db)
+        {
+            db.Database.EnsureDeleted();
+            Console.WriteLine("Database was successfully deleted!");
+            db.Database.EnsureCreated();
+            Console.WriteLine("Database was successfully created!");
+        }
+
+        // Problem 02
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            User[] users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+
+        // Problem 03
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Count}";
+        }
+
+        // Problem 04
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            var categories = JsonConvert.DeserializeObject<Category[]>(inputJson)
+                .Where(c => c.Name != null)
+                .ToArray();
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Length}";
+        }
+
+        // Problem 05
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            var categoriesProducts = JsonConvert.DeserializeObject<CategoryProduct[]>(inputJson);
+
+            context.CategoryProducts.AddRange(categoriesProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {categoriesProducts.Length}";
+        }
+
+        // Problem 06
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var products = context.Products.Where(p => p.Price >= 500 && p.Price <= 1000)
+                .OrderBy(p => p.Price)
+                .Select(p => new
+                {
+                    name = p.Name,
+                    price = p.Price,
+                    seller = p.Seller.FirstName + " " + p.Seller.LastName
+                })
+                .ToArray();
+
+            string json = JsonConvert.SerializeObject(products, Formatting.Indented);
+
+            return json;
         }
     }
 }
