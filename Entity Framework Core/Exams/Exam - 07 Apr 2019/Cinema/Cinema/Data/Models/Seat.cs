4c6dc61ff88da3fc206b4b7dfc28fcef7b729d33
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-08-04 01:59:58 +0300

    Add Exam "Cinema - 07 Apr 2019" Skeleton and Model Definition

---
+++ Entity Framework Core/Exams/Exam - 07 Apr 2019/Cinema/Cinema/Data/Models/Seat.cs
@@ -0,0 +1,16 @@
+﻿namespace Cinema.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Seat
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Hall")]
+        public int HallId { get; set; }
+        public virtual Hall Hall { get; set; }
+    }
+}
---
+++ Entity Framework Core/Exams/Exam - 07 Apr 2019/Cinema/Cinema/Data/Models/Ticket.cs
@@ -0,0 +1,24 @@
+﻿namespace Cinema.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        [Required]
+        [ForeignKey("Projection")]
+        public int ProjectionId { get; set; }
+        public virtual Projection Projection { get; set; }
+    }
+}
