db5ece9c1aa89673fde443e5628c812c88781eeb
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-08-04 03:02:36 +0300

    Add "JSON Import"
    -- ImportMovies,
    -- ImportHallsAndSeats

--- Entity Framework Core/Exams/Exam - 07 Apr 2019/Cinema/Cinema/DataProcessor/Deserializer.cs
+++ Entity Framework Core/Exams/Exam - 07 Apr 2019/Cinema/Cinema/DataProcessor/Deserializer.cs
@@ -1,8 +1,17 @@
 ﻿namespace Cinema.DataProcessor
 {
     using System;
-
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using Cinema.Data.Models;
+    using Cinema.Data.Models.Enums;
+    using Cinema.DataProcessor.ImportDto;
     using Data;
+    using Microsoft.EntityFrameworkCore.Internal;
+    using Newtonsoft.Json;
 
     public class Deserializer
     {
@@ -18,12 +27,103 @@
 
         public static string ImportMovies(CinemaContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
+
+            ImportMoviesDTO[] importMoviesDTOs = JsonConvert.DeserializeObject<ImportMoviesDTO[]>(jsonString);
+
+            List<Movie> movies = new List<Movie>();
+
+            foreach (var importMovieDTO in importMoviesDTOs)
+            {
+                if (!IsValid(importMovieDTO))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                if (movies.Any(x => x.Title == importMovieDTO.Title))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                if (!Enum.TryParse(typeof(Genre), importMovieDTO.Genre, out object genre))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                var movie = new Movie
+                {
+                    Title = importMovieDTO.Title,
+                    Genre = (Genre)Enum.Parse(typeof(Genre), importMovieDTO.Genre),
+                    Duration = importMovieDTO.Duration,
+                    Rating = importMovieDTO.Rating,
+                    Director = importMovieDTO.Director
+                };
+
+                movies.Add(movie);
+                sb.AppendLine(String.Format(SuccessfulImportMovie, movie.Title, movie.Genre, movie.Rating.ToString("f2")));
+            }
+
+            context.Movies.AddRange(movies);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportHallSeats(CinemaContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
+
+            ImportHallAndSeatDTO[] hallAndSeatDTOs = JsonConvert.DeserializeObject<ImportHallAndSeatDTO[]>(jsonString);
+
+            List<Hall> halls = new List<Hall>();
+
+            foreach (var hallSeatDTO in hallAndSeatDTOs)
+            {
+                if (!IsValid(hallSeatDTO))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                var hall = new Hall
+                {
+                    Name = hallSeatDTO.Name,
+                    Is4Dx = hallSeatDTO.Is4Dx,
+                    Is3D = hallSeatDTO.Is3D,
+                };
+
+                for (int i = 0; i < hallSeatDTO.Seats; i++)
+                {
+                    hall.Seats.Add(new Seat());
+                }
+
+                halls.Add(hall);
+
+                var projectionType = "";
+
+                if (hall.Is4Dx)
+                {
+                    projectionType = hall.Is3D ? "4Dx/3D" : "4Dx";
+                }
+                else if (hall.Is3D)
+                {
+                    projectionType = "3D";
+                }
+                else
+                {
+                    projectionType = "Normal";
+                }
+
+                sb.AppendLine(String.Format(SuccessfulImportHallSeat, hall.Name, projectionType, hall.Seats.Count));
+            }
+
+            context.Halls.AddRange(halls);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportProjections(CinemaContext context, string xmlString)
@@ -34,6 +134,14 @@
         public static string ImportCustomerTickets(CinemaContext context, string xmlString)
         {
             throw new NotImplementedException();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
         }
     }
 }
---
+++ Entity Framework Core/Exams/Exam - 07 Apr 2019/Cinema/Cinema/DataProcessor/ImportDto/ImportMoviesDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace Cinema.DataProcessor.ImportDto
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ImportMoviesDTO
+    {
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        [Range(1.0, 10.0)]
+        public double Rating { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string Director { get; set; }
+    }
+}
