85b6b3b085663910d934f7df6f1387750f9ae0ed
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-10-09 18:13:12 +0300

    StartUp, IMvcApplication

--- CSharp Web/SUS/Apps/MyFirstMvcApp/Controllers/UsersController.cs
+++ CSharp Web/SUS/Apps/MyFirstMvcApp/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 {
     using SUS.HTTP;
     using SUS.MvcFramework;
+    using System;
 
     public class UsersController : Controller
     {
@@ -13,6 +14,16 @@
         public HttpResponse Register(HttpRequest request)
         {
             return this.View();
+        }
+
+        public HttpResponse DoLogin(HttpRequest request)
+        {
+            // TODO: read data
+            // TODO: check user
+            // TODO: log user
+            // TODO: redirect home page
+
+            return this.Redirect("/");
         }
     }
 }
--- CSharp Web/SUS/SUS.MvcFramework/Host.cs
+++ CSharp Web/SUS/SUS.MvcFramework/Host.cs
@@ -7,8 +7,13 @@
 
     public static class Host
     {
-        public static async Task CreateHostAsync(List<Route> routeTable, int port = 80)
+        public static async Task CreateHostAsync(IMvcApplication application, int port = 80)
         {
+            // TODO: {controller}/{action}/{id}
+            List<Route> routeTable = new List<Route>();
+            application.ConfigureServices();
+            application.Configure(routeTable);
+
             IHttpServer server = new HttpServer(routeTable);
 
             Process.Start(@"C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe", "http://localhost");
